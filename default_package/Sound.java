43e10653939b022eee0aac9b71c2f874ef7a5200
BuildTools <unconfigured@null.spigotmc.org>
2017-04-06 14:10:40 -0400

    Build01

---
+++ default_package/Sound.java
@@ -0,0 +1,20 @@
+package default_package;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.AudioSystem;
+
+public class Sound {
+
+	public void playSound(String Sound, float Volume){
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Sound.class.getResource(Sound)));
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			gainControl.setValue(Volume);
+			clip.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ default_package/test.java
@@ -0,0 +1,11 @@
+package default_package;
+
+public class test {
+
+	public static void main(String[] args) {
+		Control_Panel test = new Control_Panel();
+		test.open_cpanel();
+
+	}
+
+}
