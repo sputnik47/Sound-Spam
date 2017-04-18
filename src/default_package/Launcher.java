a972e4dda854d59134c554563e6bcb3b8dd2933c
Chandler Bone <blitzkreig2.199@gmail.com>
2017-04-18 13:22:05 -0400

    Update Launcher.java

--- src/default_package/Launcher.java
+++ src/default_package/Launcher.java
@@ -23,8 +23,9 @@ public class Launcher {
 		
 		System.out.println("Running...");
 		if(save.readSave(6).equals("true") || save.readSave(1).equals("true")){
+			save.writeSave(6, "false");
 			ghost.start();
-			save.writeSave(6, "false");}
+			}
 			
 			
 	}
